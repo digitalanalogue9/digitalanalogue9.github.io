fd55b7ef54de0731d923e117d6d16cd115431f9b
Dan Hibbert <Dan.Hibbert@gmail.com>
2024-10-26 18:57:17 +0100

    updated to next@latest

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    output: 'export', // For GitHub Pages static export
+    images: {
+      unoptimized: true
+    },
+    basePath: process.env.NODE_ENV === 'production' ? '/digitalanalogue9.github.io' : ''
+  }
+  
+  module.exports = nextConfig
+  
