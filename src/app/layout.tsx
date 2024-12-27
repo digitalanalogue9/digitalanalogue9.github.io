f50eefb924ac9c8a12c7efabd2f67b9e5f1a2adb
Dan Hibbert <Dan.Hibbert@gmail.com>
2024-12-27 10:01:33 +0000

    updated icons again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,15 +37,29 @@ export default function RootLayout({
       <meta name="format-detection" content="telephone=no" />
       <meta name="mobile-web-app-capable" content="yes" />
       <meta name="msapplication-tap-highlight" content="no" />
-      <meta name="msapplication-TileColor" content="#3b82f6" />
       <meta property="og:url" content={process.env.NEXT_PUBLIC_SERVER_URL || 'https://core-values.me'} />
       <meta property="og:title" content="Core Values" />
       <meta property="og:description" content="Discover and prioritise your personal values" />
-      <meta property="og:image" content="https://core-values.me/icons/icon-512x512.png" />
+      <meta property="og:image" content="https://core-values.me/icons/icon-192x192.png" />
       <meta property="og:site_name" content="Core Values" />
       <link rel="manifest" href="/manifest.webmanifest" type="application/manifest+json" />
-      <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
       <link rel="shortcut icon" href="/icons/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="57x57" href="/icons/apple-icon-57x57.png" />
+      <link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-icon-60x60.png" />
+      <link rel="apple-touch-icon" sizes="72x72" href="/icons/apple-icon-72x72.png" />
+      <link rel="apple-touch-icon" sizes="76x76" href="/icons/apple-icon-76x76.png" />
+      <link rel="apple-touch-icon" sizes="114x114" href="/icons/apple-icon-114x114.png" />
+      <link rel="apple-touch-icon" sizes="120x120" href="/icons/apple-icon-120x120.png" />
+      <link rel="apple-touch-icon" sizes="144x144" href="/icons/apple-icon-144x144.png" />
+      <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-icon-152x152.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-icon-180x180.png" />
+      <link rel="icon" type="image/png" sizes="192x192" href="/icons/android-icon-192x192.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+      <meta name="msapplication-TileColor" content="#1d4ed8" />
+      <meta name="msapplication-TileImage" content="/icons/ms-icon-144x144.png" />
+      <meta name="theme-color" content="#1d4ed8" />
       <title>Core Values</title>
       <Suspense>
         <GoogleAnalytics GA_MEASUREMENT_ID={'G-JZPX5JCP5D'} />
